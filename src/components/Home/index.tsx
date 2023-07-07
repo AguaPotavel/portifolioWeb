3b0d30ab333b2f550c2ae9625b414187236039c3
Ruan Carlos <ruancarlosmeira@gmail.com>
2023-07-07 17:49:13 -0300

    :construction: atualizado home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,35 @@
-import { Container, Title, TitleWrapper } from "./styles";
+import {
+  Container,
+  Title,
+  TitleWrapper,
+  SubtitleWrapper,
+  Subtitle,
+  NameWrapper,
+} from "./styles";
 
 export default function Home() {
+  const variantsTitle = {
+    hidden: { x: -100, rotate: -90 },
+    show: {
+      x: 0,
+      rotate: 0,
+    },
+  };
+
   return (
     <Container>
-      <TitleWrapper>
+      <SubtitleWrapper>
+        <Subtitle>Hi, I'm</Subtitle>
+        <NameWrapper
+          whileHover={{
+            scale: 1.2,
+            transition: { duration: 1 },
+          }}
+        >
+          <Subtitle className="name">Ruan</Subtitle>
+        </NameWrapper>
+      </SubtitleWrapper>
+      <TitleWrapper variants={variantsTitle} initial="hidden" animate="show">
         <Title>FullStack Developer</Title>
       </TitleWrapper>
     </Container>
--- src/components/Home/styles.tsx
+++ src/components/Home/styles.tsx
@@ -10,6 +10,7 @@ export const Container = styled.div`
   height: 100vh;
   background-color: ${(props) => props.theme.colors.shape.main};
   color: ${(props) => props.theme.colors.text.main};
+  overflow: hidden;
 `;
 
 export const TitleWrapper = styled(motion.div)`
@@ -29,10 +30,70 @@ export const Title = styled.h1`
   font-family: ${(props) => props.theme.fonts.primary};
   pointer-events: none;
   user-select: none;
+  line-height: 6rem;
 
   @media (max-width: 768px) {
     font-size: 5rem;
   }
 `;
 
-export const Subtitle = styled.h2``;
+export const SubtitleWrapper = styled(motion.div)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: fit-content;
+  max-width: 80vw;
+  overflow: hidden;
+`;
+
+export const NameWrapper = styled(motion.div)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: fit-content;
+  overflow: visible;
+  position: relative;
+
+  &:hover {
+    .name {
+      color: ${(props) => props.theme.colors.text.dark};
+      z-index: 1;
+    }
+
+    &:before {
+      width: 100%;
+    }
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    width: 1%;
+    height: 100%;
+    left: 0;
+    background-color: ${(props) => props.theme.colors.primary.main};
+    transition: all 0.5s ease-in-out;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 4rem;
+  font-weight: 600;
+  color: ${(props) => props.theme.colors.text.main};
+  font-family: ${(props) => props.theme.fonts.primary};
+  pointer-events: none;
+  user-select: none;
+  line-height: 1rem;
+  margin-right: 1rem;
+
+  &.name {
+    color: ${(props) => props.theme.colors.primary.main};
+  }
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
